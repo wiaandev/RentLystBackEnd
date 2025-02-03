e545ae87131bdc2ff570ce5ef0ad91e0d0ff1482
Wiaan Duvenhage <wiaan@stackworx.io>
2025-02-03 07:33:49 +0200

    chore: added migration

---
+++ RentOutBackEnd.Presentation/Migrations/20241127095127_AddedPropertyToUserEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentOutBackEnd.Presentation.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPropertyToUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRenter",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRenter",
+                table: "AspNetUsers");
+        }
+    }
+}
